410db68dec9474633530eb2584b8d2f21e1e1b47
flightcode <flightcode@pm.me>
2022-12-01 10:24:25 +1100

    Add rice.sh

--- README.md
+++ README.md
@@ -9,12 +9,30 @@ Run `rice.sh` to install all programs/configs.
 - zsh (Shell)
 - [Starship](https://starship.rs) (Shell Prompt)
 
-## Applications
-- []() ()
-
 ## Scripts
 - [neofetch](https://github.com/dylanaraps/neofetch) (System Fetch)
+- [lolcat](https://github.com/busyloop/lolcat) (Rainbow pipe for shell)
+
+## Applications
+### Base
+- [LibreWolf](https://librewolf.net/) (Privacy-focused FireFox)
+- [BitWarden](https://bitwarden.com/) (Password Manager)
+- [ProtonVPN](https://protonvpn.com/) (VPN)
+
+### Messaging
+- [Signal](https://signal.org/) (Messaging)
+- [Discord](https://discord.com/) (Voice & Text Chat)
+
+### Development
+- [Insomnia](https://insomnia.rest/) (HTTP & GraphQL Client)
+- [VSCodium](https://github.com/VSCodium/vscodium) (Privacy-focused VSCode)
+
+### Other
+- [Obsidian](https://obsidian.md/) (Personal Knowledge Management)
+- [Calibre](https://calibre-ebook.com/) (E-Book Management)
+
 
 ## To-Do
 - [pywal](https://github.com/dylanaraps/pywal) (Colour Manager)
 - [ranger](https://github.com/ranger/ranger) (File Manager)
+- Doom EMACS/EMACS/NeoVIM?
---
+++ rice.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Install Programs
+brew install starship
+
+# Install Scripts
+brew install neofetch lolcat
+
+# Install Applications
+brew install --cask librewolf bitwarden protonvpn signal discord insomnia calibre obsidian vscodium
+
+## Install Librewolf Extensions
+echo "Install extensions..."
+open -a LibreWolf https://addons.mozilla.org/en-US/firefox/addon/bitwarden-password-manager/
+open -a LibreWolf https://addons.mozilla.org/en-US/firefox/addon/simplelogin/
+echo "Confirm..."
+read
+
+## Setup Obsidian Vault
+gh repo clone flightcode/Obsidian ~/Obsidian
+echo "Create cronjob for .sync.sh..."
+read
+echo "Allow cron full disk access in settings..."
+read
+
+## Config VSCodium
+echo "Please install 'codium' command in PATH in vscode command palette"
+read
+
+## Install VSCodium Extensions
+codium --install-extension pkief.material-icon-theme
+codium --install-extension esbenp.prettier-vscode
+codium --install-extension dbaeumer.vscode-eslint
+codium --install-extension eamodio.gitlens
+codium --install-extension visualstudioexptteam.vscodeintellicode
+codium --install-extension jpoissonnier.vscode-styled-components
+codium --install-extension yzhang.markdown-all-in-one
+
+## Update VSCode settings
+curl -fsSL https://gist.githubusercontent.com/flightcode/d1cc24a053f6d8dcd2849545ddae6e7a/raw/vscode-settings.json > ~/Library/Application\ Support/VSCodium/User/settings.json
